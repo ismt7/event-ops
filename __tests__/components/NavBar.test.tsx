f1075aef1cdd0843cf864879ccbc7a994b44e0a7
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-04-02 10:00:30 +0900

    fix

--- __tests__/components/NavBar.test.tsx
+++ __tests__/components/NavBar.test.tsx
@@ -21,4 +21,18 @@ describe("NavBar", () => {
     expect(settingsLink).toBeInTheDocument();
     expect(settingsLink).toHaveAttribute("href", "/settings");
   });
+
+  it("contains a link to the home page in the title", () => {
+    render(<NavBar />);
+    const titleLink = screen.getByText("Event Ops");
+    expect(titleLink).toBeInTheDocument();
+    expect(titleLink).toHaveAttribute("href", "/");
+  });
+
+  it("contains a link to the report page", () => {
+    render(<NavBar />);
+    const reportLink = screen.getByText("レポート");
+    expect(reportLink).toBeInTheDocument();
+    expect(reportLink).toHaveAttribute("href", "/event-report");
+  });
 });
